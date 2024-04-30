4f84c2004233997fadcecfc18f5bd02462d2a534
gowtham <gowtham05092000@gmail.com>
2024-04-30 15:21:48 +0530

    first commit

---
+++ components/Webviewer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div id="webviewer" ref="viewer"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WebViewer',
+    props: {
+        path: String,
+        url: String
+    },
+    mounted() {
+        if (typeof window !== 'undefined') {
+            import('@pdftron/webviewer').then(({ default: WebViewer }) => {
+                WebViewer({
+                    path: '../webviewer',
+                    initialDoc: this.url,
+                }, this.$refs.viewer).then(instance => {
+                    // Do something with the WebViewer instance if needed
+                }).catch(error => {
+                    console.error('Error initializing WebViewer:', error);
+                });
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1 style="text-align: center;">PDF-VIEWER</h1>
+      <WebViewer :path="`${publicPath}webviewer`" url="https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf"/>
+  </div>
+</template>
+
+<script>
+import WebViewer from '../components/Webviewer'; // Make sure to properly reference the WebViewer component
+
+export default {
+  name: 'app',
+  components: {
+      WebViewer
+  },
+  data() {
+      return {
+          publicPath: process.env.BASE_URL
+      }
+  }
+}
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+}
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 auto;
+  height: 100vh;
+}
+#webviewer {
+  height: 100vh;
+}
+</style>
